1642d007daa6a1651f7e6e20444cbe973768258b
Siddharth <siddharthmehta0906@gmail.com>
2021-06-06 12:44:57 +0530

    Update settings.py

--- Agame/settings.py
+++ Agame/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
